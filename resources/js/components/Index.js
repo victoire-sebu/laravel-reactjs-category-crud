6e5ee557eea875080ee863f2c46bc8f3fe66238f
Victoire Sebu <vitechsebu@gmail.com>
2021-02-03 23:43:21 +0100

    Add Header component

--- resources/js/components/Index.js
+++ resources/js/components/Index.js
@@ -1,21 +1,29 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import Footer from "./Footer";
+
 
 class Index extends Component {
     render() {
         return (
-            <div className="container">
-                <div className="row justify-content-center">
-                    <div className="col-md-8">
-                        <div className="card">
-                            <div className="card-header">Example Component</div>
+            <div>
+                <Header></Header>
+                <div className="container">
+                    <div className="row justify-content-center">
+                        <div className="col-md-8">
+                            <div className="card">
+                                <div className="card-header">
+                                    Example Component
+                                </div>
 
-                            <div className="card-body">
-                                I'm an example component!
+                                <div className="card-body">
+                                    I'm an example component!
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
+                
             </div>
         );
     }
