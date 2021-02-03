e1c3a8d9f914f39a86eb0cdf42ea182009ce7fa9
Victoire Sebu <vitechsebu@gmail.com>
2021-02-03 23:32:29 +0100

    Add changement fonction on class in Index comp

--- resources/js/components/Index.js
+++ resources/js/components/Index.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
 
-function Index() {
-    return (
-        <div className="container">
-            <div className="row justify-content-center">
-                <div className="col-md-8">
-                    <div className="card">
-                        <div className="card-header">Example Component</div>
+class Index extends Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-md-8">
+                        <div className="card">
+                            <div className="card-header">Example Component</div>
 
-                        <div className="card-body">
-                            I'm an example component!
+                            <div className="card-body">
+                                I'm an example component!
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default Index;
 
-if (document.getElementById('app')) {
+if (document.getElementById("app")) {
     ReactDOM.render(<Index />, document.getElementById("app"));
 }
