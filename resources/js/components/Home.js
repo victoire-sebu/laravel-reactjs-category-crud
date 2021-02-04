c8c1f9088638ce58d631acc57e525b088c546596
Victoire Sebu <vitechsebu@gmail.com>
2021-02-04 17:27:08 +0100

    Add about and home page

---
+++ resources/js/components/Home.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-md-8">
+                        <div className="card">
+                            <div className="card-body">
+                                This is the home page
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+export default Home;
