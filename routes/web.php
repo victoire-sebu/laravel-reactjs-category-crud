fe8d5bbff8077918b1593ccf680ad8c4a9f8a90e
Victoire Sebu <vitechsebu@gmail.com>
2021-02-06 08:02:22 +0100

    Add index router for category index

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\CategoryController;
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('category', [CategoryController::class, 'index']);
